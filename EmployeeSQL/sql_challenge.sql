6ca3b5e53b320f27975ca3e99563f7257b9b287e
jobongie <68445190+jobongie@users.noreply.github.com>
2020-10-07 23:26:17 -0500

    Back at it, reworked ERD, and code for DATE type and KEYS

--- EmployeeSQL/sql_challenge.sql
+++ EmployeeSQL/sql_challenge.sql
@@ -1,14 +1,40 @@
 
--- DROP TABLE dept_manager;
--- DROP TABLE titles;
--- DROP TABLE employees;
+-- -- ORDER NEEDED TO DROP TABLES DUE TO KEYS
 -- DROP TABLE salaries;
--- DROP TABLE departments;
+-- DROP TABLE dept_manager;
 -- DROP TABLE dept_emp;
+-- DROP TABLE employees;
+-- DROP TABLE titles;
+-- DROP TABLE departments;
+
 
 -- CREATE TABLE "departments" (
 --     "dept_num" VARCHAR   NOT NULL,
---     "dept_name" VARCHAR   NOT NULL
+--     "dept_name" VARCHAR   NOT NULL,
+--     CONSTRAINT "pk_departments" PRIMARY KEY (
+--         "dept_num"
+--      )
+-- );
+
+-- CREATE TABLE "titles" (
+--     "title_id" VARCHAR   NOT NULL,
+--     "title" VARCHAR   NOT NULL,
+--     CONSTRAINT "pk_titles" PRIMARY KEY (
+--         "title_id"
+--      )
+-- );
+
+-- CREATE TABLE "employees" (
+--     "emp_num" INTEGER   NOT NULL,
+--     "title_id" VARCHAR   NOT NULL,
+--     "birth_date" VARCHAR   NOT NULL,
+--     "first_name" VARCHAR   NOT NULL,
+--     "last_name" VARCHAR   NOT NULL,
+--     "sex" CHAR(1)   NOT NULL,
+--     "hire_date" DATE   NOT NULL,
+--     CONSTRAINT "pk_employees" PRIMARY KEY (
+--         "emp_num"
+--      )
 -- );
 
 -- CREATE TABLE "dept_emp" (
@@ -21,28 +47,17 @@
 --     "emp_num" INTEGER   NOT NULL
 -- );
 
--- CREATE TABLE "titles" (
---     "title_id" VARCHAR   NOT NULL,
---     "title" VARCHAR   NOT NULL
--- );
-
--- CREATE TABLE "employees" (
---     "emp_num" INTEGER   NOT NULL,
---     "title_id" VARCHAR   NOT NULL,
---     "birth_date" VARCHAR   NOT NULL,
---     "first_name" VARCHAR   NOT NULL,
---     "last_name" VARCHAR   NOT NULL,
---     "sex" CHAR(1)   NOT NULL,
---     "hire_date" VARCHAR   NOT NULL
--- );
-
 -- CREATE TABLE "salaries" (
 --     "emp_num" INTEGER   NOT NULL,
 --     "salary" INTEGER   NOT NULL
 -- );
 
+-- ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_title_id" FOREIGN KEY("title_id")
+-- REFERENCES "titles" ("title_id");
 
--- # FIgure out composite keys and redo PK and FK
+-- ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_num" FOREIGN KEY("emp_num")
+-- REFERENCES "employees" ("emp_num");
+
 -- ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_num" FOREIGN KEY("dept_num")
 -- REFERENCES "departments" ("dept_num");
 
@@ -50,18 +65,12 @@
 -- REFERENCES "departments" ("dept_num");
 
 -- ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_num" FOREIGN KEY("emp_num")
--- REFERENCES "dept_emp" ("emp_num");
-
--- ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_num" FOREIGN KEY("emp_num")
--- REFERENCES "dept_emp" ("emp_num");
-
--- ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_title_id" FOREIGN KEY("title_id")
--- REFERENCES "titles" ("title_id");
+-- REFERENCES "employees" ("emp_num");
 
 -- ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_num" FOREIGN KEY("emp_num")
--- REFERENCES "dept_emp" ("emp_num");
+-- REFERENCES "employees" ("emp_num");
 
--- SELECT * FROM dept_emp;
+-- -- Verify using countrows under table
 
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
@@ -71,9 +80,9 @@
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE hire_date = 1986;
+-- SELECT first_name, last_name, hire_date
+-- FROM employees
+-- WHERE hire_date BETWEEN '01/01/1986' AND '01/01/1987';
 
 -- # USE CTE to pull from tables departments, dept_manager, employees. NOTE: I changed man_emp_num to emp_num as designator m is man specific
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
